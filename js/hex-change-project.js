6f7dd73c3e642845d0fa9cf18376fb7a12ef5fcd
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-21 14:58:57 -0500

    added an array and random number gen

--- js/hex-change-project.js
+++ js/hex-change-project.js
@@ -2,4 +2,21 @@
 
 (function () {
 
+    let hexColorArray = [
+        "#CD5C5C",
+        "#FF0000",
+        "#FFC0CB",
+        "#FF1493",
+        "#FF8C00",
+        "#FFFF00",
+        "#FF00FF",
+        "#FF00FF",
+        "#663399",
+        "#2E8B57",
+        "#008B8B",
+        "#00FFFF"
+    ];
+
+    let randomNumber = Math.floor(Math.random() * ((hexColorArray.length - 1) - 0 + 1));
+    console.log(randomNumber);
 })();
