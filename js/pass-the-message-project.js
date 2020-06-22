2b03d9e515a9bb61c70a4a7aef961cd018dfc1f6
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-06-21 19:35:58 -0500

    completed project

--- js/pass-the-message-project.js
+++ js/pass-the-message-project.js
@@ -2,5 +2,14 @@
 
 (function () {
 
+    // let userInput = document.getElementById("message-input").value;
+
+    document.getElementById("submit").addEventListener("click", () => {
+        let userInput = document.getElementById("message-input").value;
+        // console.log(userInput);
+        document.getElementById("message").innerText = userInput;
+        document.getElementById("message-input").value = "";
+    })
+
 
 })();
