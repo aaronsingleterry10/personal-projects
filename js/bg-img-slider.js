93b9e3f81dc38befff484c8bd52428e306fa6848
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-07-01 18:25:56 -0500

    renamed images

--- js/bg-img-slider.js
+++ js/bg-img-slider.js
@@ -2,4 +2,18 @@
 
 (function () {
 
+    let puppies = [
+        "url('/img/puppies0.jpeg')",
+        "url('/img/puppies1.jpeg')",
+        "url('/img/puppies2.jpeg')",
+        "url('/img/puppies3.jpeg')",
+        "url('/img/puppies4.jpeg')"
+    ];
+
+    let count = 0;
+
+    document.getElementById("image").style.backgroundImage = "url('img/puppies0.jpeg')";
+    document.getElementById("left-btn").addEventListener("click", () => {
+        console.log("left button clicked");
+    })
 })()
