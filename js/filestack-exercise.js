258057bf1af2516b807872b1b845345558003ad2
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-08-17 10:21:52 -0500

    pushing up

--- js/filestack-exercise.js
+++ js/filestack-exercise.js
@@ -1,10 +1,15 @@
 (function () {
 
     const client = filestack.init(FILESTACK_KEY);
-    const options = {
-        onUploadDone: res => {
-            console.log(res);
-        }
-    };
-    client.picker(options).open();
+    document.getElementById("upload").addEventListener("click", (e) => {
+        e.preventDefault();
+        const options = {
+            imageMax: [225, 225],
+            onFileUploadFinished: res => {
+                $("img").attr("src", res.url);
+            }
+        };
+        client.picker(options).open();
+    });
+
 })();
