e1fcc460327522890dd8ae792ec28e33e29d92af
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-09-22 22:23:40 -0500

    refactored

--- js/clock.js
+++ js/clock.js
@@ -4,8 +4,20 @@
 
     setInterval(() => {
         let d = new Date();
-        document.getElementById("hours").innerText = d.getHours() - 12 + " : ";
-        document.getElementById("minutes").innerText = d.getMinutes() + " : ";
-        document.getElementById("seconds").innerText = "" + d.getSeconds();
+        if (d.getHours() < 10) {
+            document.getElementById("hours").innerText = "0" + d.getHours() - 12 + " : ";
+        } else {
+            document.getElementById("hours").innerText = d.getHours() - 12 + " : ";
+        }
+        if (d.getMinutes() < 10) {
+            document.getElementById("minutes").innerText = "0" + d.getMinutes() + " : ";
+        } else {
+            document.getElementById("minutes").innerText = d.getMinutes() + " : ";
+        }
+        if (d.getSeconds() < 10) {
+            document.getElementById("seconds").innerText = "0" + d.getSeconds();
+        } else {
+            document.getElementById("seconds").innerText = "" + d.getSeconds();
+        }
     }, 1000);
 })();
