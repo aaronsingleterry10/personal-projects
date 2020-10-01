ce97010988bfe27f61662f0244c401910bcfe1ab
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-10-01 09:59:11 -0500

    added edit button

--- js/to-do-list.js
+++ js/to-do-list.js
@@ -7,8 +7,12 @@ $(document).ready(() => {
             .then(data => {
                 console.log(data)
                 $(data).each(function (index, element) {
-                    $('#todo').append(`<h1>${element.title}</h1>`);
-                    $('#todo').append(`<p>${element.description}</p>`);
+                    $('#todo').append(`
+                        <h1>${element.title}</h1>
+                        <p>${element.description}</p>
+                        <input type="submit" id="${element.id}" value="edit">
+                    `);
+                    // $('#todo').append(`<p>${element.description}</p>`);
                 });
             });
     }
@@ -21,20 +25,6 @@ $(document).ready(() => {
         let titleInput = $("#title").val();
         let descriptionInput = $("#description").val();
         console.log(titleInput, descriptionInput);
-        // let userInput = {
-        //     title: titleInput,
-        //     description: descriptionInput
-        // };
-        // let options = {
-        //     method: "POST",
-        //     headers: {
-        //         "Content-Type": "application/json"
-        //     },
-        //     body: JSON.stringify({
-        //         title: titleInput,
-        //         description: descriptionInput
-        //     })
-        // };
         fetch("http://localhost:3000/todo", {
             method: "POST",
             headers: {
@@ -55,36 +45,4 @@ $(document).ready(() => {
             });
 
     });
-
-    // let newPost = {title: "Post Title", description: "Post Description"};
-    // const options = {
-    //     method: "POST",
-    //     body: JSON.stringify(newPost),
-    //     headers: {
-    //         "Content-Type": "application/json"
-    //     }
-    // };
-    // $("#send").click(e => {
-    //     e.preventDefault();
-    //     fetch(myList, options)
-    // .then(response => response.json())
-    // .then(data => {
-    //     $(data).each(function (index, element) {
-    //         $('#todo').append(`<h1>${element.title}</h1>`);
-    //         $('#todo').append(`<p>${element.description}</p>`);
-    //     });
-    // });
-    // });
-    // $.ajax("data/list.json").done(function (data) {
-    //     let myData = JSON.stringify( { title: "my third title", description: "my third description"});
-    //     data.push(myData);
-    // });
-    // $.ajax("data/list.json").done(function (data) {
-    //    console.log(data);
-    //    $(data).each(function (index, element) {
-    //        $('#todo').append(`<h1>${element.title}</h1>`);
-    //        $('#todo').append(`<p>${element.description}</p>`)
-    //    });
-    // });
-
 });
