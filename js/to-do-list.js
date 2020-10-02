77292db3656e5d3434673dd319379717deb6e299
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-10-01 21:58:35 -0500

    refactored

--- js/to-do-list.js
+++ js/to-do-list.js
@@ -1,48 +1,60 @@
 $(document).ready(() => {
-    const myList = "json/list.json";
+    const url = "http://localhost:3000/todo";
 
     function showData() {
-        fetch("http://localhost:3000/todo")
+        fetch(url)
             .then(response => response.json())
             .then(data => {
-                console.log(data)
+                console.log(data);
+                let obj = data.filter(n => {
+                    return n.id === 3;
+                });
+                console.log(obj);
                 $(data).each(function (index, element) {
                     $('#todo').append(`
-                        <h1>${element.title}</h1>
-                        <p>${element.description}</p>
-                        <input type="submit" id="${element.id}" value="edit">
+                        <div id="${element.id}">
+                            <h3>${element.title}</h3>
+                            <p>${element.description}</p>
+                            <button data-id="${element.id}" data-action="edit">Edit</button>
+                            <button data-id="${element.id}" data-action="delete">Delete</button>
+                        </div>
                     `);
-                    // $('#todo').append(`<p>${element.description}</p>`);
                 });
             });
     }
 
     showData();
 
-    $("#send").click((e) => {
+    $("#todo").click((e) => {
         e.preventDefault();
-        console.log(e.target)
-        let titleInput = $("#title").val();
-        let descriptionInput = $("#description").val();
-        console.log(titleInput, descriptionInput);
-        fetch("http://localhost:3000/todo", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json; charset=UTF-8"
-            },
-            body: JSON.stringify({
-                title: titleInput,
-                description: descriptionInput
-            })
-        })
+        let button = e.target.dataset.action
+        fetch(url)
             .then(response => response.json())
             .then(data => {
-                console.log(data)
-                $(data).each(function (index, element) {
-                    $('#todo').append(`<h1>${element.title}</h1>`);
-                    $('#todo').append(`<p>${element.description}</p>`);
-                });
+                if (button === "edit") {
+                    let obj = data.filter(n => {
+                        return n.id === parseInt(e.target.dataset.id);
+                    });
+                    $(`#${obj[0].id}`).html(`
+                        <div class="form-group">
+                            <label for="edit-title">Title:</label>
+                            <input type="text" class="form-control" id="edit-title" value="${obj[0].title}">
+                        </div>
+                        <div class="form-group">
+                            <label for="edit-description">Description:</label>
+                            <input type="text" class="form-control" id="edit-description" value="${obj[0].description}">
+                        </div>
+                        <button data-id="${obj[0].id}" data-action="submit-changes">Submit Changes</button>
+                    `);
+                }
+                if (button === "delete") {
+                    let obj = data.filter(n => {
+                        return n.id === parseInt(e.target.dataset.id);
+                    });
+                }
+                if (button === "submit-changes") {
+                    console.log("you've clicked Submit Changes");
+                }
             });
-
     });
 });
