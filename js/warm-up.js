67f345b4a1c30667c33c092bb0c7667383a84658
Aaron Singleterry <aaronsingleterry10@gmail.com>
2021-03-31 15:10:27 -0500

    added to fetch api

--- js/warm-up.js
+++ js/warm-up.js
@@ -1,14 +1,26 @@
 (function () {
 
     // fetch("https://pokeapi.co/api/v2/pokemon/").then(response => console.log(response.json()))
-
-    fetch("https://pokeapi.co/api/v2/pokemon/")
+    let singlePokemon = [];
+    fetch("https://pokeapi.co/api/v2/pokemon/?limit=30")
         .then(response => response.json())
         .then(pokemon => {
-            // console.log(pokemon.results[3])
-            for (let x in pokemon) {
-
+            // console.log(pokemon.results[3].name)
+            for (let x in pokemon.results) {
+                // console.log(pokemon.results[x].url)
+                let result = pokemon.results[x].url
+                fetch(result)
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log(data.name)
+                        // for (let i in data) {
+                        //     console.log(data[i])
+                        // }
+                    })
             }
+            // for (let x = 0; x < pokemon.results.length; x++) {
+            //     console.log(p)
+            // }
         })
 
 })();
