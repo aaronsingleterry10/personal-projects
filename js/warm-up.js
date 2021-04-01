3aa212b95573d0ee51b5afad344b2d14de754951
Aaron Singleterry <aaronsingleterry10@gmail.com>
2021-04-01 11:27:36 -0500

    added elements

--- js/warm-up.js
+++ js/warm-up.js
@@ -12,7 +12,23 @@
                 fetch(result)
                     .then(response => response.json())
                     .then(data => {
-                        console.log(data.name)
+                        let image = data.sprites.front_default;
+                        let name = data.name;
+                        let capName = name.charAt(0).toUpperCase() + name.slice(1);
+                        // console.log(data.sprites.front_default)
+                        document.getElementById("pokemon").innerHTML +=
+                            `
+                                <div class="col">
+                                    <div class="card">
+                                        <div class="c-card"><img src="${image}" class="card-img-top" alt="..."></div>
+                                        <div class="card-body">
+                                        <h5 class="card-title">${capName}</h5>
+                                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                        </div>
+                                    </div>
+                                </div> 
+                            `
+                            // `<div> ${data.name} </div>`
                         // for (let i in data) {
                         //     console.log(data[i])
                         // }
