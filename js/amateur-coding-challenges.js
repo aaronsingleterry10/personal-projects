08811a06e9f4b8835a358320454de67914fe83e9
Aaron Singleterry <aaronsingleterry10@gmail.com>
2021-04-11 09:30:17 -0500

    added code for quiz

--- js/amateur-coding-challenges.js
+++ js/amateur-coding-challenges.js
@@ -1,67 +1,72 @@
 (function () {
 
-    function startFinishStep(start, finish, step) {
-        let num = 0;
-        for (let i = start; i <= finish; i = i + step) {
-            num = num + i;
-        }
-        return num;
-    }
+    // function startFinishStep(start, finish, step) {
+    //     let num = 0;
+    //     for (let i = start; i <= finish; i = i + step) {
+    //         num = num + i;
+    //     }
+    //     return num;
+    // }
     // console.log(startFinishStep(1, 4, 1));
     // console.log(startFinishStep(4, 10));
     // console.log(startFinishStep(3, 7, 2));
     // console.log(startFinishStep(1, 4, 2));
 
-    function hasEnglish(input) {
-        let userInput = input.toLowerCase();
-        let word = "english";
-        return userInput.indexOf(word) > 0;
-    }
+    // function hasEnglish(input) {
+    //     let userInput = input.toLowerCase();
+    //     let word = "english";
+    //     return userInput.indexOf(word) > 0;
+    // }
     // console.log(hasEnglish("SMFENgliSHasnD"));
     // console.log(hasEnglish("FaagdnglishAGG"));
 
-    function twoArrayTwoInts(array) {
-        let sumOfPosNums = 0;
-        let sumOfNegNums = 0;
-        let newArray = [];
-        for (let i = 0; i < array.length; i++) {
-            if (array[i] >= 0) {
-                sumOfPosNums += 1;
-            } else {
-                sumOfNegNums = sumOfNegNums + array[i];
-            }
-        }
-        newArray.push(sumOfPosNums, sumOfNegNums);
-        return newArray;
-    }
+    // function twoArrayTwoInts(array) {
+    //     let sumOfPosNums = 0;
+    //     let sumOfNegNums = 0;
+    //     let newArray = [];
+    //     for (let i = 0; i < array.length; i++) {
+    //         if (array[i] >= 0) {
+    //             sumOfPosNums += 1;
+    //         } else {
+    //             sumOfNegNums = sumOfNegNums + array[i];
+    //         }
+    //     }
+    //     newArray.push(sumOfPosNums, sumOfNegNums);
+    //     return newArray;
+    // }
     // let myArr = [1, -6, 5, 4, 3, -7, -10, 201, -3];
     // console.log(twoArrayTwoInts(myArr));
 
-    function translateToNum(input) {
-
-    }
-    let myString = "challenge";
-    let singleChar = myString[0];
-    let input = myString.split("");
-    let num = 0;
-    // console.log(input)
-    // console.log(input["a"]);
-    // console.log(typeof input["l"]);
-    let results = input.reduce((wordCounts, word) => {
-        if (typeof wordCounts[word] === 'undefined') {
-            console.log("wordCounts", wordCounts["l"]);
-            console.log("word: " + word);
-            console.log(wordCounts[word]);
-            wordCounts[word] = 1;
-            console.log(wordCounts[word])
-        } else {
-            console.log("wordCounts: " + wordCounts);
-            console.log("word: " + word);
-            console.log(wordCounts[word]);
-            wordCounts[word] += 1;
-            console.log(wordCounts[word])
-        }
-        return wordCounts;
-    }, []);
-    console.log(results);
+    // function translateToNum(input) {
+    //
+    // }
+    // let myString = "challenge";
+    // let singleChar = myString[0];
+    // let input = myString.split("");
+    // let num = 0;
+    // // console.log(input)
+    // // console.log(input["a"]);
+    // // console.log(typeof input["l"]);
+    // let results = input.reduce((wordCounts, word) => {
+    //     if (typeof wordCounts[word] === 'undefined') {
+    //         console.log("wordCounts", wordCounts["l"]);
+    //         console.log("word: " + word);
+    //         console.log(wordCounts[word]);
+    //         wordCounts[word] = 1;
+    //         console.log(wordCounts[word])
+    //     } else {
+    //         console.log("wordCounts: " + wordCounts);
+    //         console.log("word: " + word);
+    //         console.log(wordCounts[word]);
+    //         wordCounts[word] += 1;
+    //         console.log(wordCounts[word])
+    //     }
+    //     return wordCounts;
+    // }, []);
+    // console.log(results);
+   let a = [1,2,3,4,5];
+   let b = a.map(element => element * 2);
+   let c = b.filter(element => element > 5);
+   let d = c.length;
+   console.log(d)
 })();
